41b09dae1e3d93d25fc8fe750be58a275aa8b191
Kencho-Dorji <kenchodorji893@gmail.com>
2025-08-07 13:55:09 +0600

    numbers.py

--- first programe.py
+++ first programe.py
@@ -4,6 +4,7 @@ b=3
 sum=a+b
 print("The sum of a and b is:",sum)
 print("**************************************")
-name = input("Kencho Dorji")
-age = input("18")
-print("you are" ,name, "and you are" ,age, "years old")
+name = input ("Kencho Dorji")
+age = input (18)
+print("you are" , name "and you are" , age "years old")
+
